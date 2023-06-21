e40e7c7644308130dd0e4b39694c767dbe464b0e
hecht-a <axel.hct@gmail.com>
2023-06-21 09:33:48 +0200

    fix: typing errors

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,5 +1,5 @@
 import {ResourceLoader} from "jsdom";
-import {Options} from "got";
+import {OptionsOfTextResponseBody} from "got";
 
 export const GOOGLE_URL = "https://google.com/search?q="
 
@@ -11,14 +11,16 @@ export const RESOURCE_LOADER = new ResourceLoader({
 	userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
 });
 
-export const OPTIONS: Options = {
+export const OPTIONS: OptionsOfTextResponseBody = {
 	headers: {
 		accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 		"accept-language": "fr,fr-FR;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
 		"cache-control": "no-cache",
 		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
 	},
-	rejectUnauthorized: false,
+	https: {
+		rejectUnauthorized: false
+	},
 	method: 'get'
 }
 
