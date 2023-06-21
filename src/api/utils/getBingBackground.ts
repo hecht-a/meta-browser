aa493ada83c6db587a8be9002f441c7cca997a86
hecht-a <axel.hct@gmail.com>
2023-06-21 09:34:56 +0200

    fix: error with bing background
    quotes weren't removed

--- src/api/utils/getBingBackground.ts
+++ src/api/utils/getBingBackground.ts
@@ -3,7 +3,7 @@ import {BING_URL, OPTIONS} from "@api/constants";
 import {JSDOM} from "jsdom";
 
 async function getBing(): Promise<Document> {
-	const {body} = await got.get(`${BING_URL}`, OPTIONS)
+	const {body} = await got(`${BING_URL}`, OPTIONS)
 	const dom = new JSDOM(body)
 	return dom.window.document
 }
@@ -11,6 +11,6 @@ async function getBing(): Promise<Document> {
 export async function getBingBackground() {
 	const bing = await getBing()
 	const imgCont = bing.querySelector('.img_cont')!
-	const img = imgCont.getAttribute('style')!.split(/.+\((.+)\).+/)[1];
+	const img = imgCont.getAttribute('style')!.split(/.+\((.+)\).+/)[1].replace('"', '');
 	return `${BING_URL}${img}`
 }
